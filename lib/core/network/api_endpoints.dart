037d0be187c59465ef6e8c9f8644232cbc2427da
osama-sn <osamaessamkhalifa@gmail.com>
2026-07-21 22:09:43 +0300

    edti_1

--- lib/core/network/api_endpoints.dart
+++
@@ -1,9 +0,0 @@
-class ApiEndpoints {
-  ApiEndpoints._();
-
-  static const String baseUrl = 'https://api.example.com/v1';
-  
-  static const String login = '/auth/login';
-  static const String register = '/auth/register';
-  static const String profile = '/user/profile';
-}
